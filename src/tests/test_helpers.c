b1b85b02c946da09ebdfdda558736c3044af354e
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2026-02-19 13:57:11 +0300

    test(helpers): add my_null_decimal_garbage, my_null_decimal_already_zero

--- src/tests/test_helpers.c
+++ src/tests/test_helpers.c
@@ -41,6 +41,24 @@ START_TEST(my_is_zero_multiple_nonzero) {
 END_TEST
 
 
+START_TEST(my_null_decimal_garbage){
+    my_decimal val;
+    my_null_decimal(&val);
+    ck_assert_int_eq(val.bits[0], 0);
+    ck_assert_int_eq(val.bits[1], 0);
+    ck_assert_int_eq(val.bits[2], 0);
+    ck_assert_int_eq(val.bits[3], 0);
+}
+END_TEST
+
+START_TEST(my_null_decimal_already_zero){
+    my_decimal val = {{0, 0, 0, 0}};
+    my_null_decimal(&val);
+    ck_assert_int_eq(val.bits[0], 0);
+}
+END_TEST
+
+
 
 
 Suite* my_helpers_suite(void) {
@@ -52,6 +70,8 @@ Suite* my_helpers_suite(void) {
     tcase_add_test(tc_core, my_is_zero_fourth_bit_is_digit);
     tcase_add_test(tc_core, my_is_zero_all_zero);
     tcase_add_test(tc_core, my_is_zero_multiple_nonzero);
+    tcase_add_test(tc_core, my_null_decimal_garbage);
+    tcase_add_test(tc_core, my_null_decimal_already_zero);
 
     suite_add_tcase(s, tc_core);
     return s;
