d7d299e1812ceb7dda99fdf6b8eaaa74d5c1324c
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2026-02-19 13:57:52 +0300

    fix(headers): int -> void in foo null_decimal

--- src/headers/my_helpers.h
+++ src/headers/my_helpers.h
@@ -16,7 +16,7 @@ int my_set_scale(my_decimal* value, int scale);                    // TODO
 int my_get_bit(my_decimal value, int bit_index);                   // TODO
 int my_set_bit(my_decimal* value, int bit_index, int bit_value);   // TODO
 int my_is_zero(my_decimal value);                                  // UNDER REVIEW
-int my_null_decimal(my_decimal value);                             // TODO
+void my_null_decimal(my_decimal* value);                             // TODO
 int my_normalize_pair(my_decimal* value_1, my_decimal* value_2);  // TODO
 int my_shift_left(my_big_decimal* value, int shift);               // TODO
 int my_shift_right(my_big_decimal* value, int shift);              // TODO
