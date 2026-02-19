a08deec6df402fab79127f4dc5e2ae62cd8d0f0f
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2026-02-19 16:08:22 +0300

    feat(helpers): add my_set_bit

--- src/core/helpers.c
+++ src/core/helpers.c
@@ -24,13 +24,31 @@ void my_null_decimal(my_decimal* value) {
 }
 
 
-int my_get_bit(my_decimal value, int index_of_bit) {
-    if (index_of_bit < 0 || index_of_bit >= 96) {
+int my_get_bit(my_decimal value, int bit_index) {
+    if (bit_index < 0 || bit_index >= 96) {
         return 0;
     }
 
-    int index_word = index_of_bit / 32;
-    int bit_in_word = index_of_bit % 32;
+    int index_word = bit_index / 32;
+    int bit_in_word = bit_index % 32;
 
     return (value.bits[index_word] >> bit_in_word) &1;
+}
+
+
+
+int my_set_bit(my_decimal* value, int bit_index, int bit_value) {
+    if (!value || bit_index < 0 || bit_index >= 96) {
+        return 0;
+    }
+
+    int index_word = bit_index / 32;
+    int bit_in_word = bit_index % 32;
+
+    if (bit_value) {
+        value->bits[index_word] |= (1 << bit_in_word);
+    } else {
+        value->bits[index_word] &= ~(1 << bit_in_word);
+    }
+    return 0;
 }
