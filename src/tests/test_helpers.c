0eb7a318e6815fe25534241a23d5e758e7cab8e4
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2026-02-19 16:08:10 +0300

    test(helpers): add my_set_bit_basic

--- src/tests/test_helpers.c
+++ src/tests/test_helpers.c
@@ -1,4 +1,5 @@
 #include <check.h>
+#include <stdio.h>
 #include "../headers/my_helpers.h"
 
 
@@ -68,6 +69,19 @@ START_TEST(my_get_bit_basic){
 }
 END_TEST
 
+START_TEST(my_set_bit_basic){
+    my_decimal val = {{0, 0, 1 << 6, 0}};
+    int index_of_bit = 70;
+    int bit_value = 0;
+    printf("%u\n", val.bits[2]);
+    my_set_bit(&val, index_of_bit, bit_value);
+    printf("%u\n", val.bits[2]);
+    int result;
+    result = my_get_bit(val, index_of_bit);
+    ck_assert_int_eq(result, 0);
+}
+END_TEST
+
 Suite* my_helpers_suite(void) {
     Suite *s = suite_create("helpers");
     TCase *tc_core = tcase_create("Core");
@@ -80,6 +94,7 @@ Suite* my_helpers_suite(void) {
     tcase_add_test(tc_core, my_null_decimal_garbage);
     tcase_add_test(tc_core, my_null_decimal_already_zero);
     tcase_add_test(tc_core, my_get_bit_basic);
+    tcase_add_test(tc_core, my_set_bit_basic);
 
 
     suite_add_tcase(s, tc_core);
